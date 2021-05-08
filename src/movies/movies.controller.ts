f7a90853496e0dd5dc7857e677e0cd90f7ab9f16
tonky0110 <tonky0110@gmail.com>
2021-05-08 14:25:27 +0900

    0.0.0.4 service생성(nest g s) 서비스: 비즈니스 역할 담당.

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,25 +1,24 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { Movie } from './entities/movie.entity';
+import { MoviesService } from './movies.service';
 
 @Controller('movies')
 export class MoviesController {
-    @Get()
-    getAll() {
-        return "This will return movies";
-    }
+    constructor(private readonly moviesService: MoviesService) {}
 
-    @Get("search")
-    search(@Query("year") searchingYear: string) {
-        return `We are searching for a movie made after: ${searchingYear}`
+    @Get()
+    getAll(): Movie[] {
+        return this.moviesService.getAll();
     }
 
     @Get("/:id")
-    getOne(@Param("id") movieId: string) {
-        return `This will return one movie with the movieId: ${movieId}.`;
+    getOne(@Param("id") movieId: string): Movie {
+        return this.moviesService.getOne(movieId);
     }
 
     @Post()
     create(@Body() movieData) {
-        return movieData;
+        return this.moviesService.create(movieData);
     }
 
     @Patch("/:id")
@@ -32,7 +31,7 @@ export class MoviesController {
 
     @Delete("/:id")
     remove(@Param("id") movieId: string) {
-        return `This will delete a movie with the movieId: ${movieId}.`
+        return this.moviesService.deleteOne(movieId)
     }
 
 }
